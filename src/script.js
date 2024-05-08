83e756ac0df0ad2f3ab5e5b43860f7b9c617811d
Luciano <luciano@Xubunto.myguest.virtualbox.org>
2024-05-08 17:33:18 -0300

    build and config grid

--- src/script.js
+++ src/script.js
@@ -16,24 +16,14 @@ inputColor.setAttribute("id", "color-select")
 inputColor.setAttribute("type", "color")
 penColor.appendChild(inputColor)
 
-// const backgroundColor = document.createElement("div")
-// controlPanel.appendChild(backgroundColor)
-
-// const bgColorText = document.createElement("p")
-// bgColorText.textContent = "Pen Color"
-// backgroundColor.appendChild(penColorText)
-
-// const inputBgColor = document.createElement("input")
-// inputColor.setAttribute("id", "bg-color-select")
-// inputColor.setAttribute("type", "color")
-// backgroundColor.appendChild(inputBgColor)
-
 const eraseB = document.createElement("button");
 eraseB.textContent = "Erase";
+eraseB.setAttribute("id", "erase");
 controlPanel.appendChild(eraseB);
 
 const resetB = document.createElement("button");
 resetB.textContent = "Reset";
+resetB.setAttribute("id", "reset");
 controlPanel.appendChild(resetB);
 
 const colorSelected = document.getElementById("color-select");
@@ -42,4 +32,64 @@ let color = colorSelected.value;
 colorSelected.addEventListener("input", function(){
     color = colorSelected.value;
 });
+
+//build and config divs for receive color.
+const gridContainer = document.querySelector(".grid-container")
+
+let isMouseDown = false;
+
+for (let i = 0; i < 16 * 16; i++){
+    let gridCell = document.createElement("div");
+
+    gridCell.classList.add("grid-cell");
+    
+    gridCell.addEventListener("mousedown", function(){
+            isMouseDown = true;
+            this.style.backgroundColor = color;
+    });
+
+    gridCell.addEventListener("mousemove", function(){
+        if (isMouseDown){
+            this.style.backgroundColor = color;
+        };
+    });
+
+    gridCell.addEventListener("mouseup", function(){
+        isMouseDown = false;
+    });
+    gridContainer.appendChild(gridCell);
+};
+
+let isErase = false;
+
+//erase, check and set change background color.
+const erase = document.getElementById("erase")
+erase.addEventListener("click", function(){
+    isErase = !isErase;
+    if (isErase){
+        color =gridContainer.style.backgroundColor;
+        erase.style.boxShadow = "inset 2px 5px 1px rgba (0, 0, 0, 0.6)";
+    } else {
+        color = colorSelected.value;
+        erase.style.boxShadow = "2px 5px 1px rgba (0, 0, 0, 0.6)";
+    }
+})
+
+//reset, if button clicked page refresh = reset colors.
+const reset = document.getElementById("reset");
+
+reset.addEventListener("click", function(){
+    location.reload();
+})
+
+
+
+// //reset, if button clicked reset colors.
+// document.getElementById("reset").addEventListener("click", function(){
+//         const gridCell = document.getElementsByClassName("gridCell");
+//         for (let i = 0; i < gridCell.length; i++){
+//         gridCell[i].style.backgroundColor = grid.style.backgroundColor;
+//         }
+// })
+
 
